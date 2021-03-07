f0ee0af39b960dfd75e1de5d34b78c91b9d65a69
Maksym Kobieliev <maksym.kobieliev@gmail.com>
2021-03-07 15:41:01 +0200

    Minor edit

--- src/App.js
+++ src/App.js
@@ -3,26 +3,30 @@ import "./App.css";
 import { Deck } from "./deck";
 import Hand from "./hand";
 
+function dealCards(hands, cardsPerHand) {
+  const deck = new Deck();
+  const res = [];
+
+  // In real life games, we draw cards by 1 for each player.
+  // We emulate the same process here (might be useful if we want to add animations later)
+  for (let cardI = 0; cardI < cardsPerHand; cardI++) {
+    for (let handI = 0; handI < hands; handI++) {
+      if (res[handI] === undefined) {
+        res[handI] = [];
+      }
+
+      res[handI].push(deck.drawRandomCard());
+    }
+  }
+
+  return res;
+}
+
 function App() {
   const [hands, setHands] = useState([]);
 
-  function dealCards(hands, cardsPerHand) {
-    const deck = new Deck();
-    const res = [];
-
-    // In real life games, we draw cards by 1 for each player.
-    // We emulate the same process here (might be useful if we want to add animations later)
-    for (let cardI = 0; cardI < cardsPerHand; cardI++) {
-      for (let handI = 0; handI < hands; handI++) {
-        if (res[handI] === undefined) {
-          res[handI] = [];
-        }
-
-        res[handI].push(deck.drawRandomCard());
-      }
-    }
-
-    setHands(res);
+  function play() {
+    setHands(dealCards(2,7));
   }
 
   return (
@@ -36,7 +40,7 @@ function App() {
 
       <div>
         <h4>Deal Button:</h4>
-        <button className="play-button" onClick={() => dealCards(2, 7)}>
+        <button className="play-button" onClick={() => play()}>
           Deal Cards
         </button>
       </div>
